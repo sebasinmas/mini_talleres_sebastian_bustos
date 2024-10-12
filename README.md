9d20cd71cf126404d5d9cc2a2113ceddd72543f2
SebaSinMas <sebastianbustos90@gmail.com>
2024-10-12 02:16:02 -0300

    Importa calculadora

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-## En este repositorio las ramas corresponden a la entrega de los mini talleres del ramo Proyecto de Aplicación
+# calculadora_de_promedio
 
-Este primer readme cumple la función de poner un archivo en el repositorio.
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const estudiantes = ref([]);
+const respuesta = ref(null);
+const nota1 = ref(1);
+const nota2 = ref(1);
+const nota3 = ref(1);
+const nota4 = ref(1);
+const nota5 = ref(1);
+const nota6 = ref(1);
+
+const fetchEstudiantes = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/Estudiante');
+    estudiantes.value = response.data;
+    console.log(response);
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error fetching estudiantes:', error);
+  }
+};
+
+onMounted(() => {
+  fetchEstudiantes();
+});
+
+const validarNotas = (notas) => {
+  return notas.every(nota => nota >= 1.0 && nota <= 7.0);
+};
+
+const calcularPromedio = (notas) => {
+  let suma = 0;
+  suma += notas[0] * 0.2;
+  suma += notas[1] * 0.2;
+  suma += notas[2] * 0.2;
+  suma += notas[3] * 0.2;
+  suma += notas[4] * 0.1;
+  suma += notas[5] * 0.1;
+
+  return suma;
+};
+
+const procesar = () => {
+  const notas = [nota1.value, nota2.value, nota3.value, nota4.value, nota5.value, nota6.value];
+  if (validarNotas(notas)) {
+    respuesta.value = calcularPromedio(notas).toFixed(2);
+  } else {
+    respuesta.value = 'Las notas deben ser del 1.0 al 7.0';
+  }
+};
+</script>
+
+<template>
+  <div class="app-body">
+    <span>Nota 1: </span>
+    <input v-model.number="nota1" type="number" step="0.1" min="1.0" max="7.0" />
+    <span>Nota 2: </span>
+    <input v-model.number="nota2" type="number" step="0.1" min="1.0" max="7.0" />
+    <span>Nota 3: </span>
+    <input v-model.number="nota3" type="number" step="0.1" min="1.0" max="7.0" />
+    <span>Nota 4: </span>
+    <input v-model.number="nota4" type="number" step="0.1" min="1.0" max="7.0" />
+    <span>Nota 5: </span>
+    <input v-model.number="nota5" type="number" step="0.1" min="1.0" max="7.0" />
+    <span>Nota 6: </span>
+    <input v-model.number="nota6" type="number" step="0.1" min="1.0" max="7.0" />
+    <button @click="procesar">Calcular Promedio</button>
+    <p>{{ respuesta }}</p>
+  </div>
+</template>
+
+<style scoped>
+.center {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.app-body {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+button {
+  max-width: min-content;
+}
+</style>
