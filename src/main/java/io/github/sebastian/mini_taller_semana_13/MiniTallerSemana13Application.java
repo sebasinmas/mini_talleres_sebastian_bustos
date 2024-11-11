57344c4e2288ef3662b01e13a93e7ddbe41655c4
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2024-11-10 23:10:23 -0300

    Chore: Inicializa el proyecto de Spring

---
+++ src/main/java/io/github/sebastian/mini_taller_semana_13/MiniTallerSemana13Application.java
@@ -0,0 +1,13 @@
+package io.github.sebastian.mini_taller_semana_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTallerSemana13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTallerSemana13Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sebastian/mini_taller_semana_13/MiniTallerSemana13ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sebastian.mini_taller_semana_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTallerSemana13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
